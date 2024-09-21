0cf5ffd4f340870e0a4e127db91eb378de6d363b
vigneshviky01 <vikycharu@gmail.com>
2024-09-22 00:30:32 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// index.js
+function sum(a, b) {
+    return a + b;
+}
+
+module.exports = sum;
---
+++ tests/sum.test.js
@@ -0,0 +1,6 @@
+// tests/sum.test.js
+const sum = require('../index');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
